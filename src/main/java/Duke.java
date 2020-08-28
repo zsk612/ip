2b2afef9e0210ea5094746ed11b9808b9bbd950d
zsk612 <zhangsk612@gmail.com>
2020-08-28 23:22:00 +0800

    Revert "Add task markAsDone feature for Level-3"
    This reverts commit 1562efb7088210e4ebcdafc60123f6dbd1bec5c8.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,31 +13,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         greet();
-        ArrayList<Task> storedTasks = new ArrayList<>();
+        ArrayList<String> storedInfos = new ArrayList<>();
         Scanner in = new Scanner(System.in);
-        System.out.println("Tell me your plan!");
+        System.out.println("Talk with me!");
         String response = in.nextLine();
-
         while(!response.equals("bye")){
-            if ("list".equals(response)) {
-                System.out.println("---------------------");
-                displayTasks(storedTasks);
-                System.out.println("---------------------");
-            } else if (response.startsWith("done ") &&
-                       Character.isDigit(response.charAt(5))) {
-                int taskNumber = (response.charAt(5)) - '1';
-                if (taskNumber <= storedTasks.size() && taskNumber >= 1){
-                    storedTasks.get(taskNumber).setDone();
-                    System.out.println("---------------------");
-                    displayTasks(storedTasks);
-                    System.out.println("---------------------");
-                }
+            if(response.equals("list")) {
+                display(storedInfos);
+                System.out.println("Say something more!");
+                response = in.nextLine();
             } else {
-                System.out.println("Task stored: " + response);
-                addTask(storedTasks, response);
+                System.out.println("Information stored: " + response);
+                storedInfos.add(response);
+                System.out.println("Say something more!");
+                response = in.nextLine();
             }
-            System.out.println("What else?");
-            response = in.nextLine();
         }
         exit();
         in.close();
@@ -47,18 +37,14 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
-    public static void displayTasks(ArrayList<Task> storedTasks) {
+    public static void display(ArrayList<String> storedInfos) {
         int numOfInfo = 1;
-        for(Task storedTask : storedTasks) {
-            System.out.println(numOfInfo + ". [" + storedTask.getStatusIcon() + "] " + storedTask.description);
+        for(String storedInfo : storedInfos) {
+            System.out.println(numOfInfo + "." + storedInfo);
             numOfInfo++;
         }
     }
 
-    public static void addTask(ArrayList<Task> storedTasks, String response) {
-        Task t = new Task(response);
-        storedTasks.add(t);
-    }
 
     public static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
